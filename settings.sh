281a50647a9371b9a7b14fb9da65fa7efda82306
myen <avocadoraptor1@gmail.com>
2026-01-21 14:03:40 -0500

    git init generated django

---
+++ settings.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Dependencies
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+
+# Aliases
+alias manage="python3 manage.py"
+alias run="python3 manage.py runserver"
+alias migrate="python3 manage.py migrate"
+alias migrate="python3 manage.py makemigrations "
+alias check=".githooks/pre-push"
+
+# Hooks
+git config core.hooksPath .githooks
