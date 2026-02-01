49d0363c26a1d2935abd1dfb7aa1cd2fe91ff9cb
myen <avocadoraptor1@gmail.com>
2026-02-01 11:38:15 -0500

    feat: add django migrate cmds & docs

--- backend/env.sh
+++ backend/env.sh
@@ -8,8 +8,16 @@ pip install -r requirements.txt
 # Aliases
 alias manage="python3 manage.py"
 alias run="python3 manage.py runserver"
+# Migrations
+# applies migrations to the database
 alias migrate="python3 manage.py migrate"
+# creates new migration files based on the changes detected in your models
 alias makemigrations="python3 manage.py makemigrations"
+# displays the SQL statements for a migration. 
+alias sqlmigrate="python3 manage.py sqlmigrate"
+# lists a project’s migrations and their status.
+alias showmigrations="python3 manage.py showmigrations"
+
 alias check=".githooks/pre-push"
 
 # Hooks
