c3b52238acc132de72c798730bc4c29fccf6f574
myen <avocadoraptor1@gmail.com>
2026-02-02 18:34:45 -0500

    wip: updated schemas and adding qualities static data

--- backend/triangle_agency_server/views.py
+++ backend/triangle_agency_server/views.py
@@ -1,24 +1,48 @@
 import json
-from django.forms import model_to_dict
-from django.http import JsonResponse
-from django.shortcuts import get_object_or_404, render
-
-from .models import Mission, Mission
-from django.views import View
-from django.utils.decorators import method_decorator
+import os
+from typing import List
+from django.shortcuts import get_object_or_404
+from pydantic import computed_field
+from .models import Mission, Player, QualityAssurance
+from django.conf import settings
 from ninja import ModelSchema, NinjaAPI
 
+def load_json_data(file_name):
+    file_path = os.path.join(settings.BASE_DIR, 'triangle_agency', 'static', file_name)
+    try:
+        with open(file_path, 'r') as f:
+            data = json.load(f) # deserializes the JSON data into a Python dictionary or list
+        return data
+    except FileNotFoundError:
+        print(f"Error: The file {file_name} was not found at {file_path}")
+        return None
+
+QUALITIES = load_json_data('qualities.json')
 api = NinjaAPI()
 
+class QualityAssuranceSchema(ModelSchema):
+    @computed_field
+    def description(self) -> str:
+        return QUALITIES['qualities'][self.quality]['description'] # pyright: ignore[reportOptionalSubscript, reportAttributeAccessIssue]
+
+    class Meta:
+        model = QualityAssurance
+        fields = "__all__"
+
+class PlayerSchema(ModelSchema):
+    qas: List[QualityAssuranceSchema]
+
+    class Meta:
+        model = Player
+        fields = "__all__"
+
 class MissionSchema(ModelSchema):
+    players: List[PlayerSchema]
+
     class Meta:
         model = Mission
         fields = "__all__"
 
-class MissionSchema(ModelSchema):
-    class Meta:
-        model = Mission
-        fields = "__all__"
 
 
 @api.get("mission-public/{mission_public_id}/", response=MissionSchema)
@@ -26,12 +50,9 @@ def get_mission(request, mission_public_id: int):
     if mission_public_id is None:
         return None
     mission_public = get_object_or_404(Mission, pk=mission_public_id)
-    # Hack to recursively serialize.
-    # TODO: figure out proper way to do this with ninja
-    partial_mission = model_to_dict(mission_public)
-    print(partial_mission)
-    partial_mission["mission_id"] = 1
-    return partial_mission
+    serialized_data = MissionSchema.from_orm(mission_public).dict()
+    print("Serialized Data:", serialized_data)
+    return serialized_data
 
 @api.patch("mission-public/{mission_public_id}/")
 def patch_mission(request, mission_public_id: int):
