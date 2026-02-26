6c05884256292e991379b9dc89eb8ee8f4e20eae
Phil Shao <shaoster@gmail.com>
2026-02-25 19:59:13 -0500

    Fix typo in run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-TRIANGLE_AGENCY="${HOME}/triangle_agency"
+TRIANGLE_AGENCY="${HOME}/triangle-agency"
 UVICORN="${TRIANGLE_AGENCY}/.venv/bin/uvicorn"
 ${UVICORN} --app-dir ${TRIANGLE_AGENCY} --uds ${DOMAIN_SOCKET} server.asgi:application
